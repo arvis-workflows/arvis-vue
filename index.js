1b8c231ae3f0a2f84cea2201da5202e53af3bb88
jopemachine <jopemachine@naver.com>
2021-06-08 02:57:18 +0900

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const arvish = require("@jopemachine/arvish");
+
+const config = {
+	url: "bh4d9od16a-dsn.algolia.net/1/indexes/*/queries",
+	appId: "BH4D9OD16A",
+	apiKey: "85cc3221c9f23bfbaa4e3913dd7625ea",
+};
+
+const q = "query=" + arvish.input + "&hitsPerPage=10";
+
+arvish
+	.fetch(config.url, {
+		method: "POST",
+		headers: {
+			"x-algolia-application-id": config.appId,
+			"x-algolia-api-key": config.apiKey,
+		},
+		body: {
+			requests: [
+				{
+					indexName: "vuejs",
+					params: q,
+				},
+			],
+		},
+	})
+	.then((data) => {
+		const items = data.results[0].hits.map((x) => {
+			const result = {
+				title: x.anchor,
+				subtitle: x.anchor,
+				arg: x.url,
+				quicklookurl: x.url,
+			};
+
+			if (x.hierarchy) {
+				const hierarchy = Object.keys(x.hierarchy)
+					.filter((objKey) => Boolean(x.hierarchy[objKey]))
+					.sort();
+
+				result.title = x.hierarchy[hierarchy[hierarchy.length - 1]];
+				result.subtitle = hierarchy
+					.map((level) => x.hierarchy[level])
+					.join(" > ");
+			}
+
+			return result;
+		});
+
+		arvish.output(items);
+	});
---
+++ readme.md
@@ -0,0 +1,23 @@
+# arvis-vue
+
+> Search for Vue.js API references on vuejs.org
+
+<img src="media/screenshot.png">
+
+## Install
+
+```
+$ npm install -g arvis-vue
+```
+
+*Requires [Node.js](https://nodejs.org) 4+*
+
+
+## Usage
+
+In arvis, type `vue`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [radibit](http://radibit.com)
